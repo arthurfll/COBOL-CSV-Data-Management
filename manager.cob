579c358e2a3eb7a26d4bf6645bf41eb0b9994711
arthurfll <arthurfontenelle2017@gmail.com>
2023-12-22 22:20:46 -0300

    adicionada a tabela para treino

---
+++ manager.cob
@@ -0,0 +1,8 @@
+       identification division.
+       program-id. manager.
+       environment division.
+       data division.
+       file section.
+       working-storage section.
+       procedure division.
+       stop run.
